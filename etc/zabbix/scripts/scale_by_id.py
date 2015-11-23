17b755aaf9a65d947a698476c94453fb3330edf5
mch <mch@codeabovelab.com>
2015-11-23 16:22:09 +0800

    updated (added basicAuth)

--- etc/zabbix/scripts/scale_by_id.py
+++ etc/zabbix/scripts/scale_by_id.py
@@ -8,15 +8,18 @@ from docker.utils import kwargs_from_env
 import sys
 import os
 import json
-import subprocess
 import requests
+from requests.auth import HTTPBasicAuth
 
 @click.command()
 @click.argument('id')
 def main(id):
     try:
-        req=requests.post("http://opentsp-gateway-eureka:8761/ui/api/containers/"+id+"/scale")
+        print "http://opentsp-gateway-eureka:8761/ui/api/containers/"+id+"/scale"
+
+        req=requests.post("http://opentsp-gateway-eureka:8761/ui/api/containers/"+id+"/scale", auth=HTTPBasicAuth('admin', 'password'))
         print req
+        print(req.text)
     except Exception, e:
         print "can't schedule container {0} ".format(e)
 
